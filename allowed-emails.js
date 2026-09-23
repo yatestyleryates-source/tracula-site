971813831cada3264b9478d86ac951bdf62930d0
yatestyleryates-source <yatestyleryates@gmail.com>
2026-09-22 21:07:51 -0500

    Add files via upload

--- allowed-emails.js
+++ allowed-emails.js
@@ -2,6 +2,7 @@
 // If you change this list, make the same change in firestore.rules, then re-upload both files
 // and publish the rules again in the Firebase console.
 export const ALLOWED_EMAILS = [
+  "yatestyleryates@gmail.com",
   "tyates@nationsbest.net",
   "tvanbibber@nationsbest.net"
 ];
