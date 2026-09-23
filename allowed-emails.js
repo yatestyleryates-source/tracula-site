9eababbd75dc6f1db8399ea25f943063810ff161
yatestyleryates-source <yatestyleryates@gmail.com>
2026-09-22 20:59:43 -0500

    Add files via upload

---
+++ allowed-emails.js
@@ -0,0 +1,7 @@
+// Only these addresses can open the leads page and read submissions.
+// If you change this list, make the same change in firestore.rules, then re-upload both files
+// and publish the rules again in the Firebase console.
+export const ALLOWED_EMAILS = [
+  "tyates@nationsbest.net",
+  "tvanbibber@nationsbest.net"
+];
